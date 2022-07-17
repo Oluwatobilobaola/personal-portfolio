3ce18eb9a3fa5648eace5e6c958fcc9a8d0d87f6
olaide <olaideoluwatobiloba1@gmail.com>
2022-07-17 19:05:01 +0100

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Awonuga Olaide",
+    "role": "Frontend Developer ",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/oluwatobiloba-olaide-15102a200",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Oluwatobilobaola",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/tech_Olaide/status/1529783259760033795?s=20&t=6XPJixGABQ_nogpzA9DtOQ",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a front end developer based in Nigeria, from a real estate agent  to to lines of code on a web page.i love combining the worlds of logic and creative design to  make eye-catching,    accessibe and user friendly website and applications I am outgoing, dedicated, and  open minded. I get across to people and adjust to changes  with ease. I believe that a person should work on developing their professional skills and  learning new things all the time. Currently, I am  looking for new career opportunities my current job position cannot provide. I am excited to make the leap and continue refining my skills with the right company.",
+    "address":" Nigeria",
+    "website":"+234 8135095594",
+    "education":[
+      {
+        "UniversityName":"Tai Solarin University of Education",
+        "specialization":"Geography and Environmental Management.",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2017",
+        "Achievements":"BSc/Ed"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Tradedepot",
+        "specialization":"Telesales",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"black Girls In Tech (BGIT)",
+        "specialization":"Frontend",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"till date",
+        "Achievements":"HTML, CSS, JAVASCRIPT, REACT"
+      }
+    ],
+    "skillsDescription":"HTML, CSS, JAVASCRIPT, REACT.",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"There is no doubt in my mind that without the Black girls in Tech, I would not have been able to make the leap to the tech world. I started in 2021, the projects I got through the Black Girls in Tech made it possible for me to have somthing to build on. I have done so many projects on my own.",
+        "name":"Olaide"
+      },
+      {
+        "description":"70% of my contracts comes  from those who have seen my work on twitter, linkedin and github. ",
+        "name":"Olaide"
+      }
+    ]
+  }
+  
+  export default resumeData
